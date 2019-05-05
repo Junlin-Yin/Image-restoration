65f78ad590bb02adad3364a2429771bcb90fbdd4
Junlin-Yin <yjl1217265@163.com>
2019-05-05 16:47:27 +0800

    using vscode

--- code/genCorrImg.py
+++ code/genCorrImg.py
@@ -3,33 +3,36 @@ from imageio import imread, imsave
 import scipy.stats as stats
 normpdf = stats.norm.pdf
 
+
 def im2double(im):
     info = np.iinfo(im.dtype)
     return im.astype(np.double) / info.max
 
+
 def imwrite(im, filename):
     img = np.copy(im)
     img = img.squeeze()
-    if img.dtype==np.double:
-        #img = np.array(img*255, dtype=np.uint8)
+    if img.dtype == np.double:
+        # img = np.array(img*255, dtype=np.uint8)
         img = img * np.iinfo(np.uint8).max
         img = img.astype(np.uint8)
     imsave(filename, img)
 
+
 orifolder = '../ori'
 datafolder = '../data'
-samples = {'1':0.8, '2':0.4, '3':0.6}
+samples = {'1': 0.8, '2': 0.4, '3': 0.6}
 for testName, noiseRatio in samples.items():
-    Img = im2double(imread('{}/{}.png'.format(orifolder,testName)))
-    Img[(Img==0)]=0.001
-    ############### generate corrupted image ###############
+    Img = im2double(imread('{}/{}.png'.format(orifolder, testName)))
+    Img[(Img == 0)] = 0.001
+    # ############## generate corrupted image ###############
 
     if len(Img.shape) == 2:
         # only black-white images
         Img = Img[:, :, np.newaxis]
     rows, cols, channels = Img.shape
 
-    ## generate noiseMask and corrImg
+    # generate noiseMask and corrImg
     noiseMask = np.ones((rows, cols, channels))
     subNoiseNum = round(noiseRatio * cols)
     for k in range(channels):
@@ -38,5 +41,4 @@ for testName, noiseRatio in samples.items():
             noiseIdx = np.array(tmp[:subNoiseNum])
             noiseMask[i, noiseIdx, k] = 0
     corrImg = Img * noiseMask
-    imwrite(corrImg, '{}/{}.png'.format(datafolder,testName))
-    
+    imwrite(corrImg, '{}/{}.png'.format(datafolder, testName))
